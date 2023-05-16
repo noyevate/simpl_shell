163a0d9f01b587e8f61a34dbf3b426bcf7b3cb76
samuel <samuelnoye35@gmail.com>
2023-05-16 07:28:30 -0700

    0. Betty would be proud

---
+++ myshell.h
@@ -0,0 +1,17 @@
+#ifndef _MYSHELL_H_
+#define _MYSHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int sum( int numA, int numB);
+
+#endif
---
+++ test_betty.c
@@ -0,0 +1,14 @@
+#include "myshell.h"
+
+/**
+ * sum - sum togeter two integers
+ * @numA:  the first integer to be added
+ * @numB:  the second interger to be added
+ *
+ * Return: result of the suum
+ */
+
+int sum(int numA, int numB)
+{
+	return (numA + numB);
+}
